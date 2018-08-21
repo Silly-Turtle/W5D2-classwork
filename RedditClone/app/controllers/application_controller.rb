d9b6a6d1f81825b9eab2409275ff9e67a21d8f00
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-21 13:36:41 -0400

    Initiate project and finish auth

---
+++ RedditClone/app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  def login!(user)
+    @current_user = user
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logout!
+    @current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+     # redirect_to something unless logged_in? TODO
+ end
+end
---
+++ RedditClone/db/migrate/20180821173551_create_subs.rb
@@ -0,0 +1,6 @@
+class CreateSubs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subs do |t|
+    end
+  end
+end
